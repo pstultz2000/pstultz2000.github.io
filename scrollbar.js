c44b16f8ca5e8a21075f9e0635bd82aa1d0bc827
pstultz2000 <59813630+pstultz2000@users.noreply.github.com>
2026-08-27 15:23:38 -0700

    Update scrollbar.js

--- scrollbar.js
+++ scrollbar.js
@@ -1,24 +1,22 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let currentFact = "Welcome to VisitPaul.com!";
     let recentFacts = [];
-    const maxRecentFacts = 30;
+    const maxRecentFacts = 32;
 
 const facts = [
-    "Paul spent many hours creating and hosting this site...",
     "My uncle thought I was a genius for putting PB into my cereal.",
-    "I have spent hours on this scrolling bar and its fun facts alone.",
+    "I spent hours creating this scrolling bar.",
     "My networking/HTML classes were way easier than making this site.",
     "Paul got 'Most Memorable' in the yearbook his senior year.",
     "The prior host of visitpaul.com was an attorney.",
     "When I was a kid, my guinea pig did a backflip- I named him 'X'.",
     "My first laptop was a near mint Tandy 1400LT, still have it.",
     "Paul beat Pokemon Diamond only using a Bidoof.",
-    "I also own the website squid.pizza",
+    "I also own the website squid.pizza.",
     "Initial development of this site began mid 2023.",
-    "So far, Paul has attended four different colleges.",
-    "I used to play the Alto Sax- once I became skilled, I quit.",
-    "Much of this website's work was completed at night.",
-    "2005-2015 was peak computing. I still miss Windows 7...",
+    "I've attended four different colleges; I studied at two UO campuses.",
+    "I used to play the alto sax- once I became skilled, I quit.",
+    "2005-2015 was peak computing. PCs did only what you wanted.",
     "People often call my Camaro 'Bumblebee'.",
     "Random algorithms are not truly random.",
     "For my 23rd birthday, I bought what I wanted on my 13th B-Day.",
@@ -28,10 +26,9 @@ const facts = [
     "I have done way more barrel rolls than cartwheels in my life.",
     "The original site logo, a GIF, was removed for flashing too fast.",
     "Inspired by 'The Useless Web', I wanted a cool website since I was 15.",
-    "As an adult, I went half a year without eating a desert.",
+    "As an adult, I went half a year without eating a dessert.",
     "It would take over 10 minutes to view all my fun facts in one visit.",
-    "I prefer my coffee unsweetened or extremely sweetened- no inbetween.",
-    "I have probably spent thousands of hours playing video games.",
+    "I prefer my coffee unsweetened or extremely sweetened- no in between.",
     "Since I was a kid, I have wanted a classic car.",
     "I was just a few minutes away from being born in November.",
     "One time, I willfully drank a quart of kefir in under 10 minutes.",
@@ -40,10 +37,7 @@ const facts = [
     "During class, a friend gave me a jar of peanut butter for my B-Day.",
     "I have a history of switching between various operating systems.",
     "For two weeks, I did hundreds of pushups a day.",
-    "Flossing is perhaps my least favorite daily task.",
-    "I love white chocolate, but not with Fruity Pebbles inside.",
-    "The first beard I grew was a pair of mutton chops."
-
+    "The first beard I grew was a pair of mutton chops.",
     
 ];
 
